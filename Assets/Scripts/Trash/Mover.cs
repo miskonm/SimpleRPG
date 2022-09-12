ae575a2e62212b487affe22c1fdb5a07b8dc0db4
Nikita Misko <miskonm@gmail.com>
2022-09-12 21:03:20 +0300

    Add simple raycasting

--- Assets/Scripts/Trash/Mover.cs
+++ Assets/Scripts/Trash/Mover.cs
@@ -8,9 +8,24 @@ namespace SimpleRPG.Trash
         [SerializeField] private Transform _target;
         [SerializeField] private NavMeshAgent _agent;
 
+        private Camera _cachedCamera;
+
+        private void Start()
+        {
+            _cachedCamera = Camera.main;
+        }
+
         private void Update()
         {
-            _agent.destination = _target.position;
+            if (Input.GetMouseButtonDown(0))
+                MoveToCursor();
+        }
+
+        private void MoveToCursor()
+        {
+            Ray ray = _cachedCamera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit))
+                _agent.destination = hit.point;
         }
     }
 }
